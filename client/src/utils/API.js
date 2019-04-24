19d3076e8021f02378f96f4ef6a12b68dd66adc8
Kevin Boyle <kevin.michael.boyle@gmail.com>
2019-04-23 20:58:22 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,22 +4,18 @@ const APIKEY = "&api-key=Sp2JcVEAl0G3fBnSmGwNsaTm6nM5u3b5";
 
 export default {
     search: function(query, begin, end) {
-
         return axios.get(BASEURL + query + "&begin_date=" + begin + "&end_date=" + end + APIKEY);
     },
     searchTermOnly: function(query) {
-
         return axios.get(BASEURL + query + APIKEY);
     },
     getArticles: function() {
         return axios.get("/api/articles");
     },
     saveArticle: function(articleData) {
-
         return axios.post("/api/articles", articleData);
     },
     deleteArticle: function(id) {
-
         return axios.delete("/api/articles/" + id);
     }
 };
